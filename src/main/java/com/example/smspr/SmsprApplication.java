638429bb32bd14727140d661598bfe8e32e22824
JihwanPark <qazx3218@naver.com>
2024-07-09 15:27:40 +0900

    Initial commit

---
+++ src/main/java/com/example/smspr/SmsprApplication.java
@@ -0,0 +1,13 @@
+package com.example.smspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsprApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/smspr/controller/DefaultPageController.java
@@ -0,0 +1,21 @@
+package com.example.smspr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("")
+@Controller
+public class DefaultPageController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+
+}
